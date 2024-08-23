2748cbb8be4040e9cf425e9185f5e23fa4ba0f67
Matthieu Minier <matthieu.minier75@gmail.com>
2024-08-23 20:29:14 +1000

    4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: function to display a string using puts
+ * Return: should return 0;
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
