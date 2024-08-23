a85193ad8f25a123af12ee8fc9163ed3b6530f25
Matthieu Minier <matthieu.minier75@gmail.com>
2024-08-23 20:30:43 +1000

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: function to display a string using printf
+ * Return: should return 0;
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
