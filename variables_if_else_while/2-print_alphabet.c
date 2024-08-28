6265de6e9c8579c80fd262a4a2b74b449b056ca9
Matthieu Minier <matthieu.minier75@gmail.com>
2024-08-28 19:32:23 +1000

    edit description

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 
 /**
 * main - Entry point
-* Description: 'This function is to display a message using puts'
+* Description: print the alphabet in lowercase followed by a new line
 * Return: Always 0 (Success)
 */
 int main(void)
