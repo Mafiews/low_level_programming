000f11961b134da36105b78f235f52df5666ae37
Matthieu Minier <matthieu.minier75@gmail.com>
2024-09-09 00:00:40 +1000

    initial attemp

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+# include <unistd.h>
+# include <stdio.h>
+
+/**
+ * main - check the code.
+ * _islower: checks for lowercase character.
+ * returns 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c);
+
+int _islower(int c)
+{
+	
+}
+
+int main(void)
+{
+	return (0);
+}
+
+/** using std lib
+*
+*
+*/
+
