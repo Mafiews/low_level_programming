5121c0a417205d9d73dcafdb5e9ba7802f2d7dcc
Matthieu Minier <matthieu.minier75@gmail.com>
2024-09-14 22:48:56 +1000

    ex 3 print numbers ok

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers from 0 to 9, followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_numbers();
+	return (0);
+}
