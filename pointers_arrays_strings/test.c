111ab29f4759b963bf46cf2b06e70bb66276107c
Matthieu Minier <matthieu.minier75@gmail.com>
2024-09-17 18:28:25 +1000

    printing variable addresses

--- pointers_arrays_strings/test.c
+++ pointers_arrays_strings/test.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - using sizeof to dynamically determine the size of types char, int and float
+ * main - addresses of variables
  * Return: Always 0.
  */
 
 int main(void)
 {
-   int n; 
+   char c;
+   int n;
 
-   printf("Size of type 'char' on my computer: %lu bytes\n", sizeof(char));
-   printf("Size of type 'int' on my computer: %lu bytes\n", sizeof(int));
-   printf("Size of type 'float' on my computer: %lu bytes\n", sizeof(float));
-   printf("Size of type of my variable n on my computer: %lu bytes\n", sizeof(n));
+   printf("Address of variable 'c': %p\n", &c);
+   printf("Address of variable 'n': %p\n", &n);
    return (0);
 }
