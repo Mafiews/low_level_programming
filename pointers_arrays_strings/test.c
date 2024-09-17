708d99e59324acb5bc16d651e53ce72096d68db5
Matthieu Minier <matthieu.minier75@gmail.com>
2024-09-17 18:45:50 +1000

    assigning a variable's address as a pointer's value

--- pointers_arrays_strings/test.c
+++ pointers_arrays_strings/test.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - printing the address of a pointer
+ * main - storing the address of a variable into a pointer.
+ * Because &n gives us the address of the variable n,
+ * the variable p now holds the address of the variable n: p points to n.
+ * If the variable n’s address were 26, then the value of our pointer p would be 26.
+ * A pointer can only point to a variable of the type it is supposed to point to.
  * Return: Always 0.
  */
 
 int main(void)
 {
+	int n;
 	int *p;
 
-	printf("The address of the *p pointer is: %p.\n", &p);
+	n = 98;
+	p = &n;
+
+	printf("Address of the variable 'n': %p.\n", &n);
+	printf("Value of 'p': %p.\n", p);
+	
 	return (0);
 }
