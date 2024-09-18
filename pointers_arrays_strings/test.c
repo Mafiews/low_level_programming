d5ab6480468ffda670a98f49b76bd6483176e9d5
Matthieu Minier <matthieu.minier75@gmail.com>
2024-09-18 16:21:26 +1000

    pointers arithmetic 🤯

--- pointers_arrays_strings/test.c
+++ pointers_arrays_strings/test.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - printing size and address of an array
- *  In the context of sizeof, the name of the array refers to the array object itself
- * (composed of all its elements). Thus, sizeof an array
- * will give you the amount of memory space used by all its elements.
- *  In the context of &, the name of the array refers to the array object itself
- * (composed of all its elements). So the address-of the array name
- * will give you the address of the whole array, which is the same as the address of its first element.
+ * main - pointers arithmetic
  * Return: Always 0.
  */
 
@@ -15,10 +9,18 @@ int main(void)
 {
 	int a[5];
 
-	printf("Size of the array (memore space used by all elements, here 5x4 bytes): %lu.\n\n", sizeof(a));
-	printf("Printing the array and its address (object itself):\n");
-	printf("- array a: %p;\n", a);
-	printf("- address(&b): %p;\n", &a);
-	printf("The address of the whole array is the same as the address of its first element.");
+	*a = 98; /* same as doing *(a + 0) = 98 */
+	*(a + 1) = 198;
+	*(a + 2) = 298;
+	*(a + 3) = 398;
+	*(a + 4) = 498;
+
+	printf("Value of a[0] using a[0]: %d.\n", a[0]);
+	printf("Value of a[0] using *a: %d.\n", *a);
+	printf("------------------------------\n");
+	printf("Value of 'a' (also address of a[0]): %p\n", a);
+	printf("Address of 'a[1] using (a + 1)': %p\n", (a + 1));
+	printf("Address of 'a[1] using &(*(a + 1))': %p\n", &(*(a + 1)));
+	
 	return (0);
 }
