993ee17789bcb887223a41f30093582bc3e01377
Matthieu Minier <matthieu.minier75@gmail.com>
2024-09-20 21:45:46 +1000

    print value of a string

--- pointers_arrays_strings/test.c
+++ pointers_arrays_strings/test.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - Creates an array of chars and prints it
+ * main - Prints the value of a string
  * Return: Always 0.
  */
 
 int main(void)
 {
-	char a[9];
+	char a[10] = "Holberton";
 
-	*a = 'H';
-	*(a + 1) = 'o';
-	*(a + 2) = 'l';
-	a[3] = 'b';
-	*(a + 4) = 'e';
-	a[5] = 'r';
-	a[6] = 't';
-	a[7] = 'o';
-	a[8] = 'n';
-	printf("%c%c%c%c%c%c%c%c%c\n", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]);
+	printf("%s\n", a);
+	printf("Value of the last char of a: %d\n", a[9]);
+	printf("Value of a: %p\n", a);
+	printf("Value of \"Holberton\": %p\n", "Holberton");
 	return (0);
 }
